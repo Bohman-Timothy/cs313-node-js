9d7e2fe4ae803aedbd68cc3243414e5ad8670eb2
asquire <32476451+asquire@users.noreply.github.com>
2018-11-17 00:13:56 -0500

    Postage calculation app: Everything is in place expect for the amounts for each mail type and calculation of postage.

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/postage', calculatePostage)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+function calculatePostage(req, res) {
+    console.log("Calculating postage");
+    var weight = Number(req.query.weight);
+    var mailType = req.query.mailType;
+    console.log("Weight: " + weight + ", Mail Type: " + mailType);
+    var postage = 0;
+    switch (mailType) {
+        case "Letters (Stamped)":
+            break;
+        case "Letters (Metered)":
+            break;
+        case "Large Envelopes (Flats)":
+            break;
+        case "First-Class Package Service—Retail":
+            break;
+        default:
+            console.log("No mail type was selected");
+    }
+    res.render('pages/postage', {
+        weight: weight,
+        mailType: mailType,
+        postage: postage
+    });
+}
